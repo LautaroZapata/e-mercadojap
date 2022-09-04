174dd342ecc01adc731f5aca5965344c817cd24c
LautaroZapata <oratualzc@gmail.com>
2022-09-04 19:26:06 -0300

    entrega 2 finished

--- js/products.js
+++ js/products.js
@@ -1,10 +1,12 @@
+
+
 let productsArray = [];
 
 function showProductsList(array){
     let htmlContentToAppend = "";
 
-    for(let i = 0; i < array.products.length; i++){ 
-        let products = array.products[i];
+    for(let i = 0; i < array.length; i++){ 
+        let products = array[i];
         htmlContentToAppend += `
         <div class="list-group-item list-group-item-action">
             <div class="row">
@@ -28,6 +30,56 @@ function showProductsList(array){
     }
 }
 
+function filter(array){
+    let min = parseInt(document.getElementById('minCost').value);
+    let max = parseInt(document.getElementById('maxCost').value);
+    let filterMinMax = array.filter(producto => producto.cost >= min && producto.cost <= max);
+    showProductsList(filterMinMax)
+}
+
+
+let minPrice = undefined;
+let maxPrice = undefined;
+const ORDER_ASC_BY_PRICE = "AZ";
+const ORDER_DESC_BY_PRICE = "ZA";
+const ORDER_BY_PROD_SELLS = "";
+let currentProductsArray = [];
+let currentSortCriteria = undefined;
+
+function sortProducts(criteria, array){
+    let result = [];
+    if (criteria === ORDER_ASC_BY_PRICE)
+    {
+        result = array.sort((a, b)=> {
+            return a.cost - b.cost;
+        });
+    }else if (criteria === ORDER_DESC_BY_PRICE)
+    {
+        result = array.sort((a,b)=> {
+            return b.cost - a.cost;
+        });
+    }else if (criteria === ORDER_BY_PROD_SELLS)
+    {
+        result = array.sort((a,b)=> {
+            return b.soldCount - a.soldCount;
+        });
+    }
+
+    return result;
+}
+
+function sortAndShowProducts(sortCriteria, productsArray){
+    currentSortCriteria = sortCriteria;
+
+    if(productsArray != undefined){
+        currentProductsArray = productsArray;
+    }
+
+    currentProductsArray = sortProducts(currentSortCriteria, currentProductsArray);
+
+    showProductsList(currentProductsArray);
+}
+
 
 document.addEventListener("DOMContentLoaded", function(e){
     let getId = localStorage.getItem("catID");
@@ -35,9 +87,38 @@ document.addEventListener("DOMContentLoaded", function(e){
     .then(function(resultObj){
         if (resultObj.status === "ok")
         {
-            productsArray = resultObj.data;
+            productsArray = resultObj.data.products;
             showProductsList(productsArray);
+            let catN = resultObj.data.catName;
+            function mostrarCategoria(){
+                document.getElementById("prodcat").innerHTML += "Verás aquí todos los productos de la categoría " + catN;
+            };
+            mostrarCategoria();
         }
+        document.getElementById('filterBtn').addEventListener('click', () => {
+            filter(productsArray)
+        })
+
+        document.getElementById('clearFilterBtn').addEventListener('click', () => {
+            let min = document.getElementById('minCost');
+            let max = document.getElementById('maxCost');
+            min.value = "";
+            max.value = "";
+            showProductsList(productsArray)
+        })
+
+        document.getElementById("sortAsc").addEventListener("click", function(){
+            sortAndShowProducts(ORDER_ASC_BY_PRICE, productsArray);
+        });
+    
+        document.getElementById("sortDesc").addEventListener("click", function(){
+            sortAndShowProducts(ORDER_DESC_BY_PRICE);
+        });
+    
+        document.getElementById("sortByCount").addEventListener("click", function(){
+            sortAndShowProducts(ORDER_BY_PROD_SELLS);
+        });
+        
     });
     let usuario = localStorage.getItem('user');
 
